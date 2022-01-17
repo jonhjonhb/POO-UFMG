68615f9f7023fd417cc5109f55231ae5aa1fc79a
Daniel Alves <danielresende2000@gmail.com>
2022-01-17 14:55:50 -0300

    Implementação da classe Ponto3D

--- AV7/Exercício 1/Ponto3D.h
+++ AV7/Exercício 1/Ponto3D.h
@@ -6,7 +6,7 @@ class Ponto3D: public Ponto2D{
         Ponto3D(double xx = 0, double yy = 0, double zz=0): Ponto2D(xx, yy), z(zz) { };
         friend std::ostream& operator<< (std::ostream &op, const Ponto3D &p);
         Ponto3D& operator= (const Ponto3D &p);
-        void set(double nx = 0, double ny = 0, double nz=0);
+        void set(double nx = 0, double ny = 0, double nz=0) { set(nx, ny); z=nz; }
         double get_z(void) { return z; }
             
     
@@ -14,3 +14,15 @@ class Ponto3D: public Ponto2D{
         double z;
 };    
 
+std::ostream& operator<< (std::ostream &op, const Ponto3D &p){
+            op << std::endl;
+            op << "x = " << p.get_x() << std::endl;
+            op << "y = " << p.get_y() << std::endl;
+            op << "z = " << p.z << std::endl;
+            return op;
+}
+
+Ponto3D& Ponto3D::operator= (const Ponto3D &p){
+    this->set(p.get_x(), p.get_y(), p.get_z());
+    return *this;
+}
