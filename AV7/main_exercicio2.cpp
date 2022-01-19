395d7e3f4dad7b0eef8affce7cc549f13fb9b970
Daniel Alves <danielresende2000@gmail.com>
2022-01-19 15:31:34 -0300

    Correção pequena na main do exercício 2

--- AV7/main_exercicio2.cpp
+++ AV7/main_exercicio2.cpp
@@ -7,7 +7,7 @@ int main()
   Ponto3D p5(0,2,5), p6(1,2,5); // Vertices superiores da traseira 
   Ponto3D p7(0,0,5), p8(1,0,5); // Vertices Inferiores da traseira 
   
-  Box a1(p1, p2, p3, p4, p5, p6, p7, p7);
+  Box a1(p1, p2, p3, p4, p5, p6, p7, p8);
   std::cout << "Largura do objeto: " << a1.getLargura() << std::endl;
   std::cout << "Altura do objeto: " << a1.getAltura() << std::endl;
   std::cout << "Profundidade do objeto: " << a1.getProfundidade() << std::endl;  
