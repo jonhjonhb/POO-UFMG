f2d35a4dc40bd81efa87d364dcd4afb5aa5cc4bf
Johnatan Augusto <augustosales2010@gmail.com>
2022-01-26 12:28:57 -0300

    Tirei os comentarios desnecessários do arquivo main

--- AV8/main.cpp
+++ AV8/main.cpp
@@ -1,12 +1,4 @@
 #include "Agenda.h"
-/*
-Crie um programa para testar para a Agenda. Peça para o usuário informar (via teclado) quantas pessoas ele deseja colocar na agenda, e crie uma Agenda com esta informação.
-
-imprima na tela a quantidade de amigos e de conhecidos na agenda.
-adicione informações à agenda.
-imprima todos os aniversários dos amigos presentes na agenda.
-imprima todos os e-mails dos conhecidos armazenados na agenda.
-*/
 
 int main() {  
   int qntPessoas = 0;
