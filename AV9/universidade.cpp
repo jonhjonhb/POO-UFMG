1752b597c38cd14f7860e44ecfec6dac09095ab8
Johnatan Augusto <augustosales2010@gmail.com>
2022-02-09 22:21:57 -0300

    Criação da class Universidade e seus metodos.

--- AV9/universidade.cpp
+++ AV9/universidade.cpp
@@ -1,5 +1,9 @@
+#ifndef UN_H
+#define UN_H
+
 #include<string>
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
@@ -31,3 +35,28 @@ class ProfIntegral: public Professor {
     double getSalario() { return salarioMensal; };
     virtual ~ProfIntegral(){}
 };
+
+class Universidade {
+  private:
+    std::vector<Professor*> funcionarios;
+  public:
+    void addProfessor(Professor *a) { funcionarios.push_back(a); };
+    double totalPago() const;
+    void imprimeProfessores() const; 
+};
+
+double Universidade::totalPago() const {
+  double soma = 0;
+  for( Professor *a : funcionarios){
+    soma += a->getSalario();
+  }
+  return soma;
+}
+
+void Universidade::imprimeProfessores() const{
+  for( Professor *a : funcionarios){
+    cout << a->getName() << endl;
+  }
+}
+
+#endif
