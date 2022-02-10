b44a35838b068540d526145be0cacf71920579a2
Daniel Alves <danielresende2000@gmail.com>
2022-02-10 18:38:35 -0300

    Implementando métodos básicos

--- AV9/funcao.h
+++ AV9/funcao.h
@@ -2,8 +2,7 @@
 #include <cmath>
 using namespace std;
 
-class Funcao
-{
+class Funcao{
     public:
 
     //funcao que obtem a integral da funcao pela regra do trapezio
@@ -11,11 +10,48 @@ class Funcao
 
     // funcao virtual representando a funcao cuja integral deve ser calculada
     virtual double func(double input) = 0;
-
-    // destrutor
-    //virtual ~Funcao(){}
-
 };
+
+class Linear: public Funcao{
+    private: 
+        double A, B;
+        double func(double input);
+    public:
+        Linear(double _a, double _b): A(_a), B(_b){}
+};
+
+class Quadratica: public Funcao{
+    private:
+        double A, B, C;
+        double func(double input);
+    public:
+        Quadratica(double _a, double _b, double _c): A(_a), B(_b), C(_c){}
+};
+
+class Senoide: public Funcao{
+    private:
+        double func(double input);
+    public:
+        Senoide(){}
+};
+
+double Linear::func(double input){
+    return input*A + B;
+}
+
+double Quadratica::func(double input){
+    return input*input*A + input*B + C;
+}
+
+double Senoide::func(double input){
+    return sin(input)/input;
+}
+
+double Funcao::getIntegral(double limiteInferior, double limiteSuperior, double intervalos){
+    for(;;){
+        
+    }
+}
 
 int main()
 {
